6004118acdb2bb9379726292e2e34758dba3b547
JulianafBandeira <julianafbbarbosa@gmail.com>
2024-02-20 18:21:12 -0300

    classesiphone

---
+++ ClassesIphone/src/models/IPhone2007.java
@@ -0,0 +1,49 @@
+package models;
+
+import interfaces.ReprodutorMusical;
+import interfaces.AparelhoTelefonico;
+import interfaces.NavegadorInternet;
+
+public class IPhone2007 implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    public void tocar() {
+        System.out.println();
+    }
+
+    public void pausar() {
+
+        System.out.println();
+    }
+
+    public void selecionarMusica(String musica) {
+
+        System.out.println();
+    }
+
+    public void ligar(String numero) {
+       System.out.println();
+    }
+
+    public void atender() {
+       System.out.println();
+    }
+
+    public void iniciarChamadaDeVoz() {
+       System.out.println();
+    }
+
+    public void exibirPagina(String url) {
+
+       System.out.println();
+    }
+
+    public void adicionarNovaAba() {
+        
+       System.out.println();
+    }
+
+    public void atualizarPagina() {
+
+       System.out.println();
+    }
+}
+
